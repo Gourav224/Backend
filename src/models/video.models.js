e45202d98049af0587312b9a767c777fc5cc1402
Gourav Kumar Dhiman <gkrdhiman@gmail.com>
2024-02-12 21:32:58 +0530

    add

---
+++ src/models/video.models.js
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+
+const videoSchema = new Schema(
+    {
+        videoFile: {
+            type: String, //clodinary url
+            required: true,
+        },
+        thumbnail: {
+            type: String, //clodinary url
+            required: true,
+        },
+        title: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        duration: {
+            type: Number, //clodinary url
+            required: true,
+        },
+        views: {
+            type: Number,
+            default: 0,
+        },
+        isPublished: {
+            type: Boolean,
+            default: true,
+        },
+        owner: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+        },
+    },
+    {
+        timestamps: true
+    }
+);
+
+
+videoSchema.plugin(mongooseAggregatePaginate);
+export const Video = mongoose.model("Video", videoSchema);
